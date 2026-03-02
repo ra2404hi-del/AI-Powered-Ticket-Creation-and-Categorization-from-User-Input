d3cfd1acd35a85335acea7c5aaba32e5e4e714a4
ra2404hi-del <ra2404hi@gmail.com>
2026-03-02 21:05:06 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100))
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50), default="User")
+
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    category = db.Column(db.String(100))        # ✅ NEW
+    priority = db.Column(db.String(20))         # ✅ NEW
+
+    status = db.Column(db.String(20), default="Open")
+
+    date_created = db.Column(db.DateTime, default=datetime.now)   # updated
+    updated_at = db.Column(db.DateTime, default=datetime.now)     # ✅ NEW
